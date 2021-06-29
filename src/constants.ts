618f440f80222431c29c2c72befa039b0ebc5f50
Mathius <devachievem@gmail.com>
2021-06-29 13:06:59 -0700

    remove unused files

--- src/constants.ts
+++
@@ -1,4 +0,0 @@
-// src/constants.ts
-export const MONO_DB_CONNECTION_STRING =
-  process.env.MONO_DB_CONNECTION_STRING ||
-  'mongodb+srv://admin:admin@cluster0.aym4a.mongodb.net/nestjs-docker?retryWrites=true&w=majority';
